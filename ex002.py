5165c6ac9afe18e246298f5f04ac7c8e9827c229
Ana Cristina Ribeiro <125993136+anacrisrb@users.noreply.github.com>
2025-09-28 12:54:06 -0300

    Delete ex002.py

--- ex002.py
+++
@@ -1,7 +0,0 @@
-""""Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas"""
-
-nome = input('Digite o seu nome: ')
-
-print('Ola, ' + nome + '!' + ' É um prazer te conhecer!')
-
-print('É um prazer te cohecer, {}!'.format(nome))
