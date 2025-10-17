0da769834a5a27c5ca797a0836022fc079039cc2
AnaCristina <anacrisribeirost@gmail.com>
2025-10-16 21:58:08 -0300

    adicionando novos exercicios

--- EXERCICIOS/ex007.py
+++ EXERCICIOS/ex007.py
@@ -3,6 +3,4 @@
 n1 = float(input("Digite a primeira nota: "))
 n2 = float(input("Digite a segunda nota: "))
 
-media = (n1+n2)/2
-
-print("A média do aluno é: ")
+print("A média entre {} e {} é igual a {:.1f}".format(n1, n2, (n1+n2) / 2))
