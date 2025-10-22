a3010a773f4d6815e12664fd4a74b6992d11742d
AnaCristina <anacrisribeirost@gmail.com>
2025-10-21 21:12:52 -0300

    adicionando ex009 e 010

--- EXERCICIOS/ex010.py
+++ EXERCICIOS/ex010.py
@@ -1,2 +1,8 @@
 """"Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos Dólares ela pode comprar."""
 # Considere: US$ = R$ 3,27
+
+saldo = float(input("Qual o seu saldo atual? R$ "))
+
+dolares = saldo/3.27
+
+print("Com R${:.2f} você pode comprar ${:.2f} USD".format(saldo,dolares))
