c9b6e8916ebbe941be09ac91bc4c6d6863bbb01e
AnaCristina <anacrisribeirost@gmail.com>
2025-10-22 20:52:37 -0300

    add ex011

--- EXERCICIOS/ex011.py
+++ EXERCICIOS/ex011.py
@@ -1,2 +1,14 @@
 """Faça um programa que leia a largura e a altura de uma parede em metros, e calcule a sua àrea e a quantidade de
-tinta necessária para pinta-lá, sabendo que cada litro de tinta, pinta uma àrea de 2mˆ2."""
+tinta necessária para pinta-lá, sabendo que cada litro de tinta pinta uma àrea de 2mˆ2."""
+
+largura = float(input("Digite a largura da parede (em metros): "))
+altura = float(input("Digite a altura da parede (em metros): "))
+
+area = largura * altura
+
+tinta = area/2
+
+print("Sua parede tem a dimensão de {}x{} e sua área é de {:.3f}m².".format(largura, altura, area))
+print("Para pintar essa parede, você precisará de {:.4}l de tinta.".format(tinta))
+
+
