2350a0d60438d047d8626719976d8d974e7f4d0f
AnaCristina <anacrisribeirost@gmail.com>
2025-10-26 09:03:37 -0300

    add ex011, ex012 e ex013

--- EXERCICIOS/ex011.py
+++ EXERCICIOS/ex011.py
@@ -8,7 +8,7 @@ area = largura * altura
 
 tinta = area/2
 
-print("Sua parede tem a dimensão de {}x{} e sua área é de {:.3f}m².".format(largura, altura, area))
-print("Para pintar essa parede, você precisará de {:.4}l de tinta.".format(tinta))
+print("Sua parede tem a dimensão de {}x{} e sua área é de {:.2f}m².".format(largura, altura, area))
+print("Para pintar essa parede, você precisará de {:.2f}l de tinta.".format(tinta))
 
 
